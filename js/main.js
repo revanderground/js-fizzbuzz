7d56687c225db50b2526e1f9d11bdb3621c720c8
vtnboolean <vnthuanx@gmail.com>
2022-06-16 17:18:21 +0200

    with boxes but incomplete

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
-const ulElement = document.querySelector('ul.list')
+const numberWrapperRow=document.getElementById('number-wrapper');
+
  
 
 for (let index = 1; index < 101 ; index++) {
-    let n=index;
-    let liElement = document.createElement('li'); 
-    liElement = n;
-    if (n % 3 == 0 && n% 5 == 0){
-        console.log("fizzbuzz"); 
-        ulElement.append("fizzubuzz"); 
 
-    } else if(n % 3 == 0){
+    let flashyBox = document.createElement('div'); 
+    flashyBox.classList.add('col-2', 'box');
+    flashyBox.innerHTML = index
+    if (index % 3 == 0 && index% 5 == 0){
+        console.log("fizzbuzz"); 
+        flashyBox = "fizzbuzz";
+        numberWrapperRow.append(flashyBox); 
+
+    } else if(index % 3 == 0){
         console.log("fizz");
-        ulElement.append("fizz"); 
-    } else if (n % 5 ==0){
+        flashyBox = "fizz";
+        numberWrapperRow.append(flashyBox); 
+    } else if (index % 5 ==0){
         console.log("buzz");
-        ulElement.append("buzz"); 
+        flashyBox = "buzz";
+        numberWrapperRow.append(flashyBox); 
     } else {
-        console.log(n);
-        ulElement.append(n); 
+        console.log(flashyBox);
+        
+        numberWrapperRow.append(flashyBox); 
     }     
 }
