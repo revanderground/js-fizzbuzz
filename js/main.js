e38b53a24fa141a30d7dbd1fb13cafaaf7420537
vtnboolean <vnthuanx@gmail.com>
2022-06-16 16:14:25 +0200

    output DOM very awful without style...

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
+const ulElement = document.querySelector('ul.list')
+ 
 
-for (let index = 1; index < 100 ; index++) {
+for (let index = 1; index < 101 ; index++) {
     let n=index;
+    let liElement = document.createElement('li'); 
+    liElement = n;
     if (n % 3 == 0 && n% 5 == 0){
-        console.log("fizzbuzz");        
+        console.log("fizzbuzz"); 
+        ulElement.append("fizzubuzz"); 
+
     } else if(n % 3 == 0){
         console.log("fizz");
+        ulElement.append("fizz"); 
     } else if (n % 5 ==0){
         console.log("buzz");
-    } else console.log(n);
-         
+        ulElement.append("buzz"); 
+    } else {
+        console.log(n);
+        ulElement.append(n); 
+    }     
 }
