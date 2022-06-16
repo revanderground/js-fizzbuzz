6233ac7c994f73ea3f56f05c8778e8e85ed82063
vtnboolean <vnthuanx@gmail.com>
2022-06-16 17:47:29 +0200

    colored boxes done

--- js/main.js
+++ js/main.js
@@ -10,15 +10,18 @@ for (let index = 1; index < 101 ; index++) {
     if (index % 3 == 0 && index% 5 == 0){
         console.log("fizzbuzz"); 
         flashyBox.innerHTML = "fizzbuzz";
+        flashyBox.classList.add('col-2','boxfizzbuzz');
         numberWrapperRow.append(flashyBox); 
 
     } else if(index % 3 == 0){
         console.log("fizz");
         flashyBox.innerHTML = "fizz";
+        flashyBox.classList.add('col-2','boxfizz');
         numberWrapperRow.append(flashyBox); 
     } else if (index % 5 ==0){
         console.log("buzz");
         flashyBox.innerHTML = "buzz";
+        flashyBox.classList.add('col-2','boxbuzz');
         numberWrapperRow.append(flashyBox); 
     } else {
         console.log(flashyBox);
